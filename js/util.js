f6cdc2a9198029c50cdfa370bcc2249e55525d92
Alexey197 <margadonnn3@gmail.com>
2020-04-07 02:20:04 +0300

    вначале сделал показ/удаление карточки

--- js/util.js
+++ js/util.js
@@ -1,8 +1,22 @@
 'use strict';
 
 (function () {
-  window.getRandomValue = function (min, max) {
-    var rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
+  var ESC_KEYCODE = 27;
+
+  window.utils = {
+    getRandomValue: function (min, max) {
+      var rand = min + Math.random() * (max + 1 - min);
+      return Math.floor(rand);
+    },
+    getRandomArray: function (arr) {
+      var startIndex = this.getRandomValue(0, arr.length - 1);
+      var endIndex = this.getRandomValue(startIndex, arr.length - 1);
+      return arr.slice(startIndex, endIndex + 1);
+    },
+    isEscEvent: function (evt, action) {
+      if (evt.keyCode === ESC_KEYCODE) {
+        action();
+      }
+    }
   };
 })();
