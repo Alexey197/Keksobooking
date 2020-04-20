1a3154340be6a6bdcdf1734c523876d74ad193c0
Alexey197 <margadonnn3@gmail.com>
2020-04-20 18:31:40 +0300

    utils upgrade

--- js/data.js
+++ js/data.js
@@ -23,34 +23,77 @@
   var MAX_ROOMS = 5;
   var MAX_GUESTS = 10;
   var LOCATION_Y = [130, 630];
-  var AVATAR = {path: 'img/avatars/user0', ext: '.png'};
-  var PIN_SIZE = {width: 50, height: 70};
+  var AVATAR = {
+    path: 'img/avatars/user',
+    preNumber: '0',
+    ext: '.png'};
+
+  var PIN_SIZE = {
+    width: 50,
+    height: 70};
+
   var mapWidth = document.querySelector('.map__pins').clientWidth;
+
+  var chooseRandomFromArray = function (array) {
+    var randomIndex = window.utils.randomInteger(array.length - 1);
+    return {
+      value: array[randomIndex],
+      index: randomIndex
+    };
+  };
+
+  // var getRandomInteger = function (max, min) {
+  //   min = min ? Math.ceil(min) : 0;
+  //   max = Math.floor(max);
+  //   return Math.floor(Math.random() * (max - min + 1)) + min;
+  // };
+
+  var generateAvatarLinks = function (amount) {
+    var notChosen = []; // массив с цифрами от 0 до amount-1 для генерации неповторяющихся адресов картинок аватарок
+    var avatarLinks = [];
+
+    // заполняем notChosen цифрами от 0 до amount-1
+    for (var j = 0; j < amount; j++) {
+      notChosen[j] = j + 1;
+    }
+
+    // заполняем массив адресами картинок в случайном порядке
+    for (var i = 0; i < amount; i++) {
+      var randomNumber = chooseRandomFromArray(notChosen);
+
+      notChosen.splice(randomNumber.index, 1);
+      // 'img/avatars/user01.png'
+      avatarLinks[i] = AVATAR.path + (AVATAR.preNumber + randomNumber.value).slice(-2) + AVATAR.ext;
+    }
+
+    return avatarLinks;
+  };
 
   var getApartments = function (count) {
     var apartments = [];
+    var avatarLinks = generateAvatarLinks(count);
 
     for (var i = 0; i < count; i++) {
       apartments.push({
         author: {
-          avatar: AVATAR.path + window.utils.getRandomValue(1, USER_COUNT) + AVATAR.ext
+          avatar: avatarLinks[i]
         },
         offer: {
           title: 'Заголовок',
           address: '600, 350',
           price: 42,
-          type: TYPE_APARTMENTS[window.utils.getRandomValue(0, TYPE_APARTMENTS.length - 1)],
-          rooms: window.utils.getRandomValue(1, MAX_ROOMS),
-          guests: window.utils.getRandomValue(1, MAX_GUESTS),
-          checkin: CHECK_TIME[window.utils.getRandomValue(0, CHECK_TIME.length - 1)],
-          checkout: CHECK_TIME[window.utils.getRandomValue(0, CHECK_TIME.length - 1)],
-          features: window.utils.getRandomArray(FEATURES),
-          description: window.utils.getRandomArray(ADS_DESCRIPTIONS),
-          photos: window.utils.getRandomArray(PHOTOS)
+          type: TYPE_APARTMENTS[window.utils.randomInteger(0, TYPE_APARTMENTS.length - 1)],
+          rooms: window.utils.randomInteger(1, MAX_ROOMS),
+          guests: window.utils.randomInteger(1, MAX_GUESTS),
+          checkin: CHECK_TIME[window.utils.randomInteger(0, CHECK_TIME.length - 1)],
+          checkout: CHECK_TIME[window.utils.randomInteger(0, CHECK_TIME.length - 1)],
+          features: window.utils.randomArray(FEATURES),
+          description: window.utils.randomArray(ADS_DESCRIPTIONS),
+          photos: window.utils.randomArray(PHOTOS)
         },
         location: {
-          x: window.utils.getRandomValue(10, mapWidth) - PIN_SIZE.width / 2,
-          y: window.utils.getRandomValue(LOCATION_Y[0], LOCATION_Y[1]) - PIN_SIZE.height
+          x: window.utils.randomInteger(10, mapWidth) - PIN_SIZE.width / 2,
+          y: window.utils.randomInteger(LOCATION_Y[0], LOCATION_Y[1]) - PIN_SIZE.height
         }
       }
       );
--- js/util.js
+++ js/util.js
@@ -4,16 +4,21 @@
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
 
+  var getRandomInteger = function (max, min) {
+    min = min ? Math.ceil(min) : 0;
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  var getRandomArray = function (arr) {
+    var startIndex = getRandomInteger(0, arr.length - 1);
+    var endIndex = getRandomInteger(startIndex, arr.length - 1);
+    return arr.slice(startIndex, endIndex + 1);
+  };
+
   window.utils = {
-    getRandomValue: function (min, max) {
-      var rand = min + Math.random() * (max + 1 - min);
-      return Math.floor(rand);
-    },
-    getRandomArray: function (arr) {
-      var startIndex = this.getRandomValue(0, arr.length - 1);
-      var endIndex = this.getRandomValue(startIndex, arr.length - 1);
-      return arr.slice(startIndex, endIndex + 1);
-    },
+    randomInteger: getRandomInteger,
+    randomArray: getRandomArray,
     isEscEvent: function (evt, action) {
       if (evt.keyCode === ESC_KEYCODE) {
         action();
